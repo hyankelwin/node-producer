13c941648e3eb679506338e16617819d1314a460
hyan-worc <hyan@worc.com.br>
2022-07-02 17:05:54 -0300

    feat: all project modify

---
+++ src/api/index.ts
@@ -0,0 +1,46 @@
+import * as express from 'express';
+import queues from '../queues';
+const router = express.Router();
+
+const getPing = async (_req, res) => {
+  const body = { message: 'hello' };
+  return res.send(body).status(200);
+};
+
+const postLog = async (req, res) => {
+  const body = { message: req.body.message };
+  await queues.log.add(body);
+  return res.send(body).status(200);
+};
+
+const postEmail = async (req, res) => {
+  const body = { message: req.body.message };
+  await queues.email.add(body);
+  return res.send(body).status(200);
+};
+
+const postCandidate = async (req, res) => {
+  const body: {
+    partyNumber: number;
+    name: string;
+    photo: string;
+  } = req.body;
+  await queues.candidate.add(body);
+  return res.send(body).status(200);
+};
+
+const postVote = async (req, res) => {
+  const body: {
+    partyNumber: number;
+  } = req.body;
+  await queues.vote.add(body);
+  return res.send(body).status(200);
+};
+
+router.get('/', getPing);
+router.post('/log', postLog);
+router.post('/email', postEmail);
+router.post('/candidate', postCandidate);
+router.post('/votes', postVote);
+
+export default router;
